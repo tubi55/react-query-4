7a8f3eca0a5a229e0723fd0d64828f5ce019af63
tubi55 <tubi55@nate.com>
2023-12-26 12:49:00 +0900

    4버전대로 리액트쿼리로 데이터 호출

---
+++ src/hooks/useUserQuery.js
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+
+const fetchUser = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/usersa');
+  return await response.json();
+};
+
+export const useUserQuery = () => {
+  return useQuery(['users'], fetchUser, {
+    refetchOnWindowFocus: false,
+    refetchOnMount: false,
+  });
+};
