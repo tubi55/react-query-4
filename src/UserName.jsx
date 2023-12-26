e331b62e8541d09328b5fd7e573d91abc2348a70
tubi55 <tubi55@nate.com>
2023-12-26 13:42:25 +0900

    state변경

--- src/UserName.jsx
+++ src/UserName.jsx
@@ -1,12 +1,17 @@
 import { useUserQuery } from './hooks/useUserQuery';
+import { useState } from 'react';
 
 export default function UserName() {
-  const result = useUserQuery(1);
-  console.log(result);
+  const { isSuccess, data } = useUserQuery(1);
+  const [Num, setNum] = useState(0);
 
   return (
     <div className="UserName">
       <h1>User Name</h1>
+      <p>{Num}</p>
+      <button onClick={() => setNum(Num + 1)}>plus</button>
+
+      <h2>{isSuccess && data.name}</h2>
     </div>
   );
 }
