7776bf293e19209fcf0b62e3e52dd5ced3e3dc91
tubi55 <tubi55@nate.com>
2023-12-27 12:11:14 +0900

    useMutate로 클라이언트에서 서버 데이터 변경 요청 가능

--- src/UserName.jsx
+++ src/UserName.jsx
@@ -2,10 +2,12 @@ import { useState } from 'react';
 import { useUpdateUser, useUserQuery } from './hooks/useUserQuery';
 
 export default function UserName() {
-  const { isSuccess, data } = useUserQuery();
+  //1- 해당 컴포넌트 마운트시 react-query훅으로 2번째를 호출 및 자동 캐싱처리
+  const { isSuccess, data } = useUserQuery(2);
   const updateUser = useUpdateUser();
   const [UserName, setUserName] = useState('');
 
+  //4- 폼 전송이벤트 발생시 input에 있는 변경할 사용자 이름과 변경할 데이터 순번을 배열로 묶어서 update커스텀 훅으로 활성화시킨 useMutaion객체를 가져와서 mutate메서드에 인수로 전달
   const handleSubmit = (e) => {
     e.preventDefault();
     updateUser.mutate([UserName, 2]);
@@ -23,13 +25,8 @@ export default function UserName() {
         />
       </form>
 
-      <ul>
-        {isSuccess ? (
-          data.map((user) => <li key={user.id}>{user.name}</li>)
-        ) : (
-          <p>Loading...</p>
-        )}
-      </ul>
+      {/* 3-useQuery훅에 의해서 캐싱반환된 데이터값 출력 */}
+      <p>{isSuccess && data.name}</p>
     </div>
   );
 }
--- src/hooks/useUserQuery.js
+++ src/hooks/useUserQuery.js
@@ -1,44 +1,45 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
-const fetchUser = async () => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/users`);
+//특정 순번의 서버 데이터 가져오는 fetching func, custom hook (useQuery)
+const fetchUser = async ({ queryKey }) => {
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/users/${queryKey[1]}`
+  );
   return await response.json();
 };
 
-//데이터목록 호출 커스텀훅
-export const useUserQuery = () => {
-  return useQuery(['users'], fetchUser, {
+//2- 컴포넌트에서 해당 훅 호출시 고유쿼리를 등록하면서 num값을 전달해서 fetching함수 호출
+//반환된 데이터값이 옵션값에 따라 caching처리되면서 반환됨
+export const useUserQuery = (num) => {
+  return useQuery(['users', num], fetchUser, {
     refetchOnWindowFocus: false,
-    refetchOnMount: false,
+    refetchOnMount: true,
     cacheTime: 1000 * 20,
     staleTime: 1000 * 0,
   });
 };
 
-//기존 서버데이터 update 함수
+//특정 순번의 서버 데이터를 변경하는 fetching func, cutom hook (useMutation)
+//순서6- 해당 호출되면 num순번의 데이터 객체에서 name값을 같이 전달된 userName 서버데이터를 실제 변경처리한다음 반환
 export const updateUser = async ([userName, num]) => {
   const response = await fetch(
     `https://jsonplaceholder.typicode.com/users/${num}`,
     {
       method: 'PATCH',
-      body: JSON.stringify({
-        name: userName,
-      }),
-      headers: {
-        'Content-type': 'application/json; charset=UTF-8',
-      },
+      body: JSON.stringify({ name: userName }),
+      headers: { 'Content-type': 'application/json; charset=UTF-8' },
     }
   );
   const result = await response.json();
-  console.log(result);
   return result;
 };
-
-//데이터 변경 커스텀훅
 export const useUpdateUser = () => {
   const queryClient = useQueryClient();
-
+  //순서5- mutate메서드 호출시 아래구문이 자동적으로 호출되면서
+  //등록된 updateUser함수 호출
   return useMutation(updateUser, {
+    //순서7- updateUser함수로 서버값 변경시 성공적으로 일어나면
+    //해당 값을 인수로 받아서 고유 쿼리키 생성하면서 캐싱처리된 값 쿼리 클라이언트로 전역 관리
     onSuccess: (args) => {
       queryClient.setQueryData(['users', args.id], args);
     },
